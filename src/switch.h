0a6d8b6770a1df6f84ee525f41997726af440ac9
Parham Alvani <parham.alvani@gmail.com>
2016-01-28 19:25:39 +0330

    Switch structure was added.

---
+++ src/switch.h
@@ -0,0 +1,24 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : switch.h
+ *
+ * [] Creation Date : 28-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#ifndef SWITCH_H
+#define SWITCH_H
+
+struct swicth {
+	const struct matching *m;
+	int in_port;
+	int out_port;
+}
+
+#endif
