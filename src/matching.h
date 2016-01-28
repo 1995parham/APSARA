16d3f7e3c15fe875fea0971442c8618fbce06bba
Parham Alvani <parham.alvani@gmail.com>
2016-01-28 19:25:11 +0330

    Matching structure was added.

---
+++ src/matching.h
@@ -0,0 +1,27 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : matching.h
+ *
+ * [] Creation Date : 28-01-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+#ifndef MATCHING_H
+#define MATCHING_H
+
+struct matching {
+	int n, m;
+	int *match;
+};
+
+const struct matching *matching_new(int n, int m, const int[] match);
+
+void matching_delete(struct matching *m);
+
+#endif
