e2dfcc424339c277ee4f226b7ccea9c49e7bdb2e
Parham Alvani <parham.alvani@gmail.com>
2016-02-02 08:56:46 +0330

    Switch header file was improved.

--- src/switch.h
+++ src/switch.h
@@ -15,10 +15,23 @@
 #ifndef SWITCH_H
 #define SWITCH_H
 
-struct swicth {
+struct sw {
 	const struct matching *m;
 	int in_port;
 	int out_port;
-}
+	int **queue;
+};
+
+struct sw *switch_new(int in_port, int out_port);
+
+void switch_set_current_matching(struct sw *s, const int match[]);
+
+const struct matching *switch_get_current_matching(const struct sw *s);
+
+void switch_put_in_quque(int in_port, int out_port, int number);
+
+void switch_proccess(void);
+
+void switch_next_matching(struct sw *s);
 
 #endif
