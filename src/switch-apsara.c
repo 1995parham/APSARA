ced0a88e788c2a60cd20dc62676c3d530191795b
Parham Alvani <parham.alvani@gmail.com>
2016-02-03 05:59:01 +0330

    start writing APSARA matching method.

---
+++ src/switch-apsara.c
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : switch-apsara.c
+ *
+ * [] Creation Date : 03-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+#include <stdlib.h>
+#include "switch.h"
+#include "matching.h"
+
+void switch_next_matching(struct sw *s)
+{
+}
