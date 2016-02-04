b70d48ff7d53c29d0c6102132207f9f3663037a2
Parham Alvani <parham.alvani@gmail.com>
2016-02-04 09:59:54 +0330

    Start testing project components.

---
+++ src/main.c
@@ -0,0 +1,23 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 04-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+#include <stdio.h>
+#include "switch.h"
+#include "matching.h"
+
+int main(int argc, char *argv[])
+{
+	struct sw *s = switch_new(4, 4);
+	matching_print(s->m, stdout);
+}
+
