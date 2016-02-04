c87a49a751653650d195e3c5ff76b4b96be0d339
Parham Alvani <parham.alvani@gmail.com>
2016-02-04 08:35:26 +0330

    matching neighbors method was added.

--- src/switch-apsara.c
+++ src/switch-apsara.c
@@ -15,6 +15,29 @@
 #include "switch.h"
 #include "matching.h"
 
+static const struct matching **neighbors_matching(const struct matching *m)
+{
+	const struct matching **res;
+	int *match;
+	int i, j;
+
+	res = malloc((m->n * (m->n - 1) / 2) * sizeof(struct matching *));
+	match = malloc(m->n * sizeof(int));
+	for (i = 0; i < m->n; i++)
+		match[i] = m->match[i];
+
+	for (i = 0; i < m->n; i++) {
+		for (j = i; j < m->n; j++) {
+			match[i] = m->match[j];
+			match[j] = m->match[i];
+			res[i * (m->n - 1) + j - i - 1] = matching_new(m->n, m->m, match);
+			match[i] = m->match[i];
+			match[j] = m->match[j];
+		}
+	}
+	return res;
+}
+
 void switch_next_matching(struct sw *s)
 {
 }
