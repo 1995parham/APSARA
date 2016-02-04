ba7d514ca0b45694841f429c2b0d8cba8baf872d
Parham Alvani <parham.alvani@gmail.com>
2016-02-04 23:27:02 +0330

    Permutation generation function was added and tested.

---
+++ src/permutation.h
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : permutation.h
+ *
+ * [] Creation Date : 04-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+#ifndef PERMUTATION_H
+#define PERMUTATION_H
+
+void permutation(int k, int *v, int size);
+
+#endif
+
